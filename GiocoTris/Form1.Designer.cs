bbffb6f048bf117a378dc264c9da9fffa72e8a01
ENRICOSCHI <fenrico472@gmail.com>
2022-11-19 12:05:48 +0100

    Aggiungere i file di progetto.

---
+++ GiocoTris/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace GiocoTris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btinizia = new System.Windows.Forms.Button();
+            this.lbgiocatore = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btinizia
+            // 
+            this.btinizia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.99F);
+            this.btinizia.Location = new System.Drawing.Point(63, 177);
+            this.btinizia.Margin = new System.Windows.Forms.Padding(4);
+            this.btinizia.Name = "btinizia";
+            this.btinizia.Size = new System.Drawing.Size(203, 70);
+            this.btinizia.TabIndex = 1;
+            this.btinizia.Text = "RESET";
+            this.btinizia.UseVisualStyleBackColor = true;
+            this.btinizia.Click += new System.EventHandler(this.btinizia_Click);
+            // 
+            // lbgiocatore
+            // 
+            this.lbgiocatore.AutoSize = true;
+            this.lbgiocatore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.99F);
+            this.lbgiocatore.Location = new System.Drawing.Point(59, 118);
+            this.lbgiocatore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbgiocatore.Name = "lbgiocatore";
+            this.lbgiocatore.Size = new System.Drawing.Size(88, 22);
+            this.lbgiocatore.TabIndex = 2;
+            this.lbgiocatore.Text = "Giocatore";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(488, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(562, 513);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(488, 38);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(120, 102);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(488, 194);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(120, 98);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(488, 351);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(120, 90);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(644, 38);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(130, 102);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(639, 194);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(135, 99);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.White;
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox7.Location = new System.Drawing.Point(631, 336);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(143, 105);
+            this.pictureBox7.TabIndex = 9;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.White;
+            this.pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox8.Location = new System.Drawing.Point(801, 38);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(110, 102);
+            this.pictureBox8.TabIndex = 10;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.White;
+            this.pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox9.Location = new System.Drawing.Point(801, 194);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(110, 94);
+            this.pictureBox9.TabIndex = 11;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.Color.White;
+            this.pictureBox10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox10.Location = new System.Drawing.Point(792, 336);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(119, 105);
+            this.pictureBox10.TabIndex = 12;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 554);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbgiocatore);
+            this.Controls.Add(this.btinizia);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btinizia;
+        private System.Windows.Forms.Label lbgiocatore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+    }
+}
+
---
+++ GiocoTris/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiocoTris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image Cerchio = Image.FromFile(@"immagine/cerchio.png");//nomina cerchio in cui riprendo il file .png
+        Image Croce = Image.FromFile(@"immagine/croce.png");//nomina croce in cui riprendo il file .png
+        int giocatore = 0;
+
+        private void btinizia_Click(object sender, EventArgs e)
+        {//azzeramento
+            cambia();
+            pictureBox10.BackgroundImage = null;
+            pictureBox2.BackgroundImage = null;
+            pictureBox3.BackgroundImage = null;
+            pictureBox4.BackgroundImage = null;
+            pictureBox5.BackgroundImage = null;
+            pictureBox6.BackgroundImage = null;
+            pictureBox7.BackgroundImage = null;
+            pictureBox8.BackgroundImage = null;
+            pictureBox9.BackgroundImage = null;
+            pictureBox10.Enabled = true;
+            pictureBox2.Enabled = true;
+            pictureBox3.Enabled = true;
+            pictureBox4.Enabled = true;
+            pictureBox5.Enabled = true;
+            pictureBox6.Enabled = true;
+            pictureBox7.Enabled = true;
+            pictureBox8.Enabled = true;
+            pictureBox9.Enabled = true;
+        }
+        private void cambia()//cambio giocatore
+        {
+            giocatore++;
+            if (giocatore == 3) giocatore = 1;
+            lbgiocatore.Text = "Giocatore " + giocatore.ToString();
+        }
+        private void controllo()
+        {
+            //se il cerchio vince orizzontale
+            if(pictureBox5.BackgroundImage == Cerchio && pictureBox2.BackgroundImage == Cerchio && pictureBox8.BackgroundImage == Cerchio)
+            {
+                MessageBox.Show("Ha vinto il cerchio");
+            }
+            else if(pictureBox3.BackgroundImage == Cerchio && pictureBox6.BackgroundImage == Cerchio && pictureBox9.BackgroundImage == Cerchio)
+            {
+                MessageBox.Show("Ha vinto il cerchio");
+            }
+            else if (pictureBox4.BackgroundImage == Cerchio && pictureBox7.BackgroundImage == Cerchio && pictureBox10.BackgroundImage == Cerchio)
+            {
+                MessageBox.Show("Ha vinto il cerchio");
+            }
+            //se il cerchio vince verticale
+            else if (pictureBox2.BackgroundImage == Cerchio && pictureBox3.BackgroundImage == Cerchio && pictureBox4.BackgroundImage == Cerchio)
+            {
+                MessageBox.Show("Ha vinto il cerchio");
+            }
+            else if (pictureBox5.BackgroundImage == Cerchio && pictureBox6.BackgroundImage == Cerchio && pictureBox7.BackgroundImage == Cerchio)
+            {
+                MessageBox.Show("Ha vinto il cerchio");
+            }
+            else if (pictureBox8.BackgroundImage == Cerchio && pictureBox9.BackgroundImage == Cerchio && pictureBox10.BackgroundImage == Cerchio)
+            {
+                MessageBox.Show("Ha vinto il cerchio");
+            }
+            //se il cerchio vince obliquo
+            else if (pictureBox2.BackgroundImage == Cerchio && pictureBox6.BackgroundImage == Cerchio && pictureBox10.BackgroundImage == Cerchio)
+            {
+                MessageBox.Show("Ha vinto il cerchio");
+            }
+            else if (pictureBox4.BackgroundImage == Cerchio && pictureBox6.BackgroundImage == Cerchio && pictureBox8.BackgroundImage == Cerchio)
+            {
+                MessageBox.Show("Ha vinto il cerchio");
+            }
+            //se il croce vince orizzontale
+            if (pictureBox5.BackgroundImage == Croce && pictureBox2.BackgroundImage == Croce && pictureBox8.BackgroundImage == Croce)
+            {
+                MessageBox.Show("Ha vinto la croce");
+            }
+            else if (pictureBox3.BackgroundImage == Croce && pictureBox6.BackgroundImage == Croce && pictureBox9.BackgroundImage == Croce)
+            {
+                MessageBox.Show("Ha vinto la croce");
+            }
+            else if (pictureBox4.BackgroundImage == Croce && pictureBox7.BackgroundImage == Croce && pictureBox10.BackgroundImage == Croce)
+            {
+                MessageBox.Show("Ha vinto la croce");
+            }
+            //se il croce vince verticale
+            else if (pictureBox2.BackgroundImage == Croce && pictureBox3.BackgroundImage == Croce && pictureBox4.BackgroundImage == Croce)
+            {
+                MessageBox.Show("Ha vinto la croce");
+            }
+            else if (pictureBox5.BackgroundImage == Croce && pictureBox6.BackgroundImage == Croce && pictureBox7.BackgroundImage == Croce)
+            {
+                MessageBox.Show("Ha vinto la croce");
+            }
+            else if (pictureBox8.BackgroundImage == Croce && pictureBox9.BackgroundImage == Croce && pictureBox10.BackgroundImage == Croce)
+            {
+                MessageBox.Show("Ha vinto la croce");
+            }
+            //se il croce vince obliquo
+            else if (pictureBox2.BackgroundImage == Croce && pictureBox6.BackgroundImage == Croce && pictureBox10.BackgroundImage == Croce)
+            {
+                MessageBox.Show("Ha vinto la croce");
+            }
+            else if (pictureBox4.BackgroundImage == Croce && pictureBox6.BackgroundImage == Croce && pictureBox8.BackgroundImage == Croce)
+            {
+                MessageBox.Show("Ha vinto la croce");
+            }
+            //se nessuno viince
+            else if(pictureBox2.Enabled==false && pictureBox3.Enabled == false && pictureBox4.Enabled == false && pictureBox5.Enabled == false && pictureBox6.Enabled == false && pictureBox7.Enabled == false && pictureBox8.Enabled == false && pictureBox9.Enabled == false && pictureBox10.Enabled == false)
+            {
+                MessageBox.Show("Non ha vinto nessuno");
+            }
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            var a = (PictureBox)sender;//la funzione è stata ampliata poi nella parte grafica
+            if (giocatore == 1)
+            {
+                a.BackgroundImage = Cerchio;//richiama l'immagine cerchio
+                a.Enabled = false;//non si può più premere
+                
+            }
+            else
+            {
+                a.BackgroundImage = Croce;//richiama l'immagine croce
+                a.Enabled = false;//non si può più premere
+
+            }
+            controllo();
+            cambia();
+        }     
+    }
+}
